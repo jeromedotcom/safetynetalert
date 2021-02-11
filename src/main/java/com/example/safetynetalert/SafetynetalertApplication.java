ae89a2409e9b1098d173a0080a05f97ed2dffaee
Jerome Dupre <jerome.dupre@axa.fr>
2021-02-11 10:43:45 +0100

    création d'un objet contenant les données json

--- src/main/java/com/example/safetynetalert/SafetynetalertApplication.java
+++ src/main/java/com/example/safetynetalert/SafetynetalertApplication.java
@@ -1,7 +1,14 @@
 package com.example.safetynetalert;
 
+import com.example.safetynetalert.model.AllDataObject;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.File;
 
 @SpringBootApplication
 public class SafetynetalertApplication {
@@ -10,4 +17,20 @@ public class SafetynetalertApplication {
         SpringApplication.run(SafetynetalertApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            ObjectMapper mapper = new ObjectMapper();
+            //mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            AllDataObject allDataObject = null;
+            try {
+            allDataObject = mapper.readValue(new File("src/main/resources/data.json"), AllDataObject.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //System.out.println(value);
+            System.out.println(allDataObject.getFirestations().get(2).getAddress());
+        };
+    }
 }
---
+++ src/main/java/com/example/safetynetalert/model/Firestation.java
@@ -0,0 +1,23 @@
+package com.example.safetynetalert.model;
+
+public class Firestation {
+
+    private int station;
+    private String address;
+
+    public int getStation() {
+        return station;
+    }
+
+    public void setStation(int station) {
+        this.station = station;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
